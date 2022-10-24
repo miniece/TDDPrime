207ac5355ca0b77b4b31b4fb85f783d5c17defba
miniece <mvelinda@icloud.com>
2022-10-24 17:05:24 -0400

    i had to change labs to understand so yee

---
+++ TDDPrime/PrimeNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDPrime
+{
+    public class PrimeNumbers
+    {
+        public int GetPrimenNum(int number)
+            {
+                List<int> primes = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+                return primes[number];
+            }
+        }
+    }
---
+++ TDDPrime/TestPrimeNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDDPrime
+{
+    public class TestPrimeNumbers
+    {
+        [Fact]
+        public void Testing()
+        {
+
+            PrimeNumbers p = new PrimeNumbers();
+            int expected = 11;
+
+
+            int actual = p.GetPrimenNum(4);
+
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
